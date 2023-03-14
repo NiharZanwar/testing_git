5642e8a55290d0d5e35bf102e7994a508188942b
Nihar Zanwar <zanwarnihar@gmail.com>
2023-03-14 20:00:14 +0530

    added new funtion

--- test.py
+++ test.py
@@ -1,6 +1,6 @@
 
 def add_2_numbers(a, b):
-    return a + b
+    return (a + b)
 
 def sub_2_numbers(a, b):
     return a - b
@@ -10,4 +10,13 @@ def test_add_2_numbers():
     assert add_2_numbers(1, 2) == 3, "Should be 3"
 
 def test_sub_2_numbers():
+    """
+    this is a test function
+    :return:
+    """
+
     assert sub_2_numbers(1, 2) == -1, "Should be -1"
+
+
+def print_hello():
+    print("hello")
