845f8888def02cd41211a294977b3a6ae50de16f
Nihar Zanwar <zanwarnihar@gmail.com>
2023-03-14 19:57:22 +0530

    Added test case and function for addition and subtraction

---
+++ test.py
@@ -0,0 +1,13 @@
+
+def add_2_numbers(a, b):
+    return a + b
+
+def sub_2_numbers(a, b):
+    return a - b
+
+
+def test_add_2_numbers():
+    assert add_2_numbers(1, 2) == 3, "Should be 3"
+
+def test_sub_2_numbers():
+    assert sub_2_numbers(1, 2) == -1, "Should be -1"
